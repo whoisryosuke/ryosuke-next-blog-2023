cb6bb2dba43d5efa7d035b1c92590d74cb689479
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2023-08-12 01:19:27 -0700

    :lipstick: tighter

--- components/dom/Glass/Glass.tsx
+++ components/dom/Glass/Glass.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
 import Box from '../Box/Box'
+import { Theme } from '@theme/index';
 
 type GlassProps = {
-
+    transparent?: boolean;
+    blur?: keyof Theme['blur'];
 }
 
 const Glass = styled(Box)<GlassProps>`
-    background: ${({theme}) => theme.colors.glass};
-    backdrop-filter: blur( 26px );
+    background: ${({theme, transparent}) => !transparent && theme.colors.glass};
+    backdrop-filter: blur(${({theme, blur}) => theme.blur[blur]});
     /* -webkit-backdrop-filter: blur( 20px ); */
     border-radius: 30px;
     /* border: 0.0625em solid rgba( 255, 255, 255, 0.18 ); */
@@ -25,11 +27,15 @@ const Glass = styled(Box)<GlassProps>`
         bottom: 0;
         left: 0;
         z-index: -2;
-        margin: -2px -1px 0 -2px;
+        margin: -1px -1px 0 -1px;
         border-radius: inherit;
         background: ${({theme}) => theme.colors.glass};
-        box-shadow: inset 2px 2px 2px rgba(255,255,255,0.25);
+        box-shadow: inset 1px 1px 1px rgba(255,255,255,0.35);
     }
 `
 
 export default Glass
+
+Glass.defaultProps = {
+    blur: 1,
+}
