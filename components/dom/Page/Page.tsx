7792cfb366e0e5180f7496ab791810ee82c5682c
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2023-08-16 15:31:43 -0700

    :fire: remove html from page and fix layout bug

--- components/dom/Page/Page.tsx
+++ components/dom/Page/Page.tsx
@@ -11,38 +11,36 @@ import A11yCheck from '../A11yCheck'
 import { StyleSheetManager } from 'styled-components'
 import isPropValid from '@emotion/is-prop-valid'
 import MainNavbar from '../MainNavbar/MainNavbar'
+import GlobalStyles from '../GlobalStyles/GlobalStyles'
 
 type Props = {
-    title: string;
-    meta?: MetaTagsProps;
     transparent?: boolean;
 }
 
-const Page = ({children, title, meta, transparent = false}: PropsWithChildren<Props>) => (
+const Page = ({children, transparent = false}: PropsWithChildren<Props>) => (
     <StyleSheetManager enableVendorPrefixes shouldForwardProp={isPropValid}>
         <ThemeProvider>
-            <Html title={title} meta={meta}>
-                <A11yCheck />
-                <Paper
-                    // display="flex"
-                    flexDirection="column"
-                    justifyContent="space-between"
-                    bg={transparent && "background"}
-                    style={{ flex: 1 }}
-                >
-                    <main>
-                        <MainNavbar />
-                        <Box as="header" position="absolute" bottom={1} left={1}>
-                            <ThemeToggle />
-                        </Box>
+            <A11yCheck />
+            <GlobalStyles />
+            <Paper
+                // display="flex"
+                flexDirection="column"
+                justifyContent="space-between"
+                bg={transparent && "background"}
+                style={{ flex: 1 }}
+            >
+                <main>
+                    <MainNavbar />
+                    <Box as="header" position="absolute" bottom={1} left={1}>
+                        <ThemeToggle />
+                    </Box>
 
-                        {children}
-                    </main>
-                    <footer>
-                        {/* <Copyright /> */}
-                    </footer>
-                </Paper>
-            </Html>
+                    {children}
+                </main>
+                <footer>
+                    {/* <Copyright /> */}
+                </footer>
+            </Paper>
         </ThemeProvider>
     </StyleSheetManager>
 )
