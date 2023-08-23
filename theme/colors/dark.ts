4554fba2033af7df26646f69f2358b062174dced
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2023-08-23 11:15:00 -0700

    :lipstick: white text on black for pressed state

--- theme/colors/dark.ts
+++ theme/colors/dark.ts
@@ -18,7 +18,7 @@ export const primaryColors = {
     default: "rgba(242, 242, 242, 0.1)",
     hovered: "rgba(242, 242, 242, 0.3)",
     pressed: "rgba(0, 0, 0, 1.0)",
-    pressedText: "rgba(242,242,242,0.1)",
+    pressedText: "rgba(242,242,242,1.0)",
   },
   primary: {
     default: "rgba(72,69,239,1)",
