3e5618c104bab61aaaa366ba61d046bd5635873f
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2023-09-11 16:39:18 -0700

    :lipstick: transition feeling better

--- components/dom/PageTransition/PageTransition.tsx
+++ components/dom/PageTransition/PageTransition.tsx
@@ -6,9 +6,36 @@ type Props = {};
 const PageTransition = ({ children }: PropsWithChildren<Props>) => {
   return (
     <motion.div
-      initial={{ opacity: 0, translateY: 300 }}
-      animate={{ opacity: 1, translateY: 0 }}
-      exit={{ opacity: 0, translateY: 300 }}
+      initial={{
+        opacity: 0,
+        translateX: "-100vw",
+        translateY: 300,
+        translateZ: 30,
+        rotateY: 30,
+        filter: "blur(30px)",
+        scale: 0.5,
+      }}
+      animate={{
+        opacity: 1,
+        translateX: 0,
+        translateY: 0,
+        translateZ: 0,
+        rotateY: 0,
+        filter: "blur(0px)",
+        scale: 1,
+      }}
+      exit={{
+        opacity: 0,
+        translateX: "-100vw",
+        translateY: 300,
+        translateZ: 30,
+        rotateY: 30,
+        filter: "blur(30px)",
+        scale: 0.5,
+      }}
+      transition={{
+        duration: 0.75,
+      }}
       style={{ width: "100%", position: "absolute", top: 0, left: 0 }}
     >
       {children}
