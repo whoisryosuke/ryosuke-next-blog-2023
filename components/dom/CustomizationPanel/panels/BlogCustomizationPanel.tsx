be24dd7f22967024db940c039745a98182c881f3
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2023-09-18 16:31:34 -0700

    :zap: wire up switch to store

--- components/dom/CustomizationPanel/panels/BlogCustomizationPanel.tsx
+++ components/dom/CustomizationPanel/panels/BlogCustomizationPanel.tsx
@@ -1,5 +1,6 @@
 import Headline from "@components/dom/Headline/Headline";
 import Slider from "@components/dom/Slider/Slider";
+import Stack from "@components/dom/Stack/Stack";
 import Switch from "@components/dom/Switch/Switch";
 import Text from "@components/dom/Text/Text";
 import { useAppStore } from "@store/app";
@@ -11,33 +12,26 @@ type Props = {};
 const BlogCustomizationPanel = (props: Props) => {
   const { customizations, setUserTheme } = useAppStore();
 
-  const fontWeightProps = customizations.theme.fontWeights;
-
-  const handleWeightChangeRegular = (e: React.FormEvent<HTMLInputElement>) => {
-    handleWeightChange(e, "regular");
-  };
-
-  const handleWeightChangeBold = (e: React.FormEvent<HTMLInputElement>) => {
-    handleWeightChange(e, "bold");
-  };
-
-  const handleWeightChange = (
-    e: React.FormEvent<HTMLInputElement>,
-    type: keyof Theme["fontWeights"]
-  ) => {
+  const handleHighContrastBlog = (e: React.FormEvent<HTMLInputElement>) => {
+    console.log("toggling contrast", e.currentTarget.checked);
     setUserTheme({
-      fontWeights: {
-        ...customizations.theme.fontWeights,
-        [type]: e.currentTarget.value,
-      },
+      highContrastBlog: e.currentTarget.checked,
     });
   };
   return (
-    <div>
-      <Headline>Customize Blog</Headline>
-      <Text>High Contrast Blog</Text>
-      <Switch />
-    </div>
+    <fieldset style={{ border: 0 }}>
+      <Headline as="legend">Customize Blog</Headline>
+      <Stack justifyContent="space-between">
+        <Text as="label" for="highContrastBlog">
+          High Contrast Blog
+        </Text>
+        <Switch
+          name="highContrastBlog"
+          checked={customizations.theme.highContrastBlog}
+          onChange={handleHighContrastBlog}
+        />
+      </Stack>
+    </fieldset>
   );
 };
 
