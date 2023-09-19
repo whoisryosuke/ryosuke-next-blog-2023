aa4cbbb959e01a996820fef494a140b5f63af8e5
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2023-09-19 15:38:18 -0700

    :zap: enable page transition for blog wrap

--- components/dom/BlogPage/BlogPage.tsx
+++ components/dom/BlogPage/BlogPage.tsx
@@ -37,90 +37,98 @@ const BlogPage = ({ children, title, ...props }: PropsWithChildren<Props>) => {
     : {};
 
   return (
-    <Box
-      width={{ mobile: "100%", tablet: "80vw" }}
-      // maxWidth="1200px"
-      margin="auto"
-      pt={{ mobile: 6, tablet: 8 }}
-      mb={{ mobile: 8, tablet: 10 }}
-      flex={1}
-      {...props}
-    >
+    <PageWrapper>
       <Box
-        display="flex"
-        flexDirection={{ mobile: "column", computer: "row" }}
-        justifyContent="center"
+        width={{ mobile: "100%", tablet: "80vw" }}
+        // maxWidth="1200px"
+        margin="auto"
+        pt={{ mobile: 6, tablet: 8 }}
+        mb={{ mobile: 8, tablet: 10 }}
+        flex={1}
+        {...props}
       >
         <Box
-          minWidth="200px"
-          maxWidth="1200px"
-          flex={1}
-          marginRight={{ mobile: 0, computer: 4 }}
-          marginBottom={{ mobile: 4, computer: 0 }}
+          display="flex"
+          flexDirection={{ mobile: "column", computer: "row" }}
+          justifyContent="center"
         >
-          <Glass p={4} borderRadius={"round"} mb={4}>
-            <Box display="flex">
-              <Button solid onlyIcon icon={<BiSidebar />} mr={3} />
-              <Button solid onlyIcon icon={<BiArrowToLeft />} disabled mr={3} />
-              <Button
-                solid
-                onlyIcon
-                icon={<BiArrowToRight />}
-                disabled
-                mr={3}
-              />
-              <Box flex={1} mr={3}>
-                <Input
-                  value="The Blog Post"
-                  icon={<BiBook />}
-                  style={{ textAlign: "center" }}
+          <Box
+            minWidth="200px"
+            maxWidth="1200px"
+            flex={1}
+            marginRight={{ mobile: 0, computer: 4 }}
+            marginBottom={{ mobile: 4, computer: 0 }}
+          >
+            <Glass p={4} borderRadius={"round"} mb={4}>
+              <Box display="flex">
+                <Button solid onlyIcon icon={<BiSidebar />} mr={3} />
+                <Button
+                  solid
+                  onlyIcon
+                  icon={<BiArrowToLeft />}
                   disabled
+                  mr={3}
                 />
+                <Button
+                  solid
+                  onlyIcon
+                  icon={<BiArrowToRight />}
+                  disabled
+                  mr={3}
+                />
+                <Box flex={1} mr={3}>
+                  <Input
+                    value="The Blog Post"
+                    icon={<BiBook />}
+                    style={{ textAlign: "center" }}
+                    disabled
+                  />
+                </Box>
+                <Button solid onlyIcon icon={<BiShare />} mr={3} />
+                <Button solid onlyIcon icon={<BiFont />} mr={3} />
+                <Button solid onlyIcon icon={<BiFolder />} mr={3} />
               </Box>
-              <Button solid onlyIcon icon={<BiShare />} mr={3} />
-              <Button solid onlyIcon icon={<BiFont />} mr={3} />
-              <Button solid onlyIcon icon={<BiFolder />} mr={3} />
-            </Box>
-          </Glass>
-          <ThemeProvider theme={customTheme}>
-            <Glass id="blog" blur={4} borderRadius={3} overflow="hidden">
-              <ScrollBox
-                overflowY="auto"
-                flex={1}
-                height="80vh"
-                borderRadius={10}
-                p={{ mobile: 5, tablet: 8 }}
-              >
-                {children}
-              </ScrollBox>
             </Glass>
-          </ThemeProvider>
-        </Box>
-        <Box minWidth="250px" opacity={{ mobile: 0, tablet: 1 }}>
-          <Glass id="toc" blur={3} overflow="hidden" p={4}>
-            <Headline id="test" fontSize={2}>
-              Table of Contents
-            </Headline>
-            <Button
-              as="a"
-              href="#test"
-              // icon={<BiTime />}
-              justifyContent="flex-start"
-            >
-              Heading 1
-            </Button>
-            <Button
-              as="a"
-              href="#test"
-              // icon={<BiTime />}
-              justifyContent="flex-start"
-            >
-              Heading 2
-            </Button>
-          </Glass>
+            <ThemeProvider theme={customTheme}>
+              <Glass id="blog" blur={4} borderRadius={3} overflow="hidden">
+                <ScrollBox
+                  overflowY="auto"
+                  flex={1}
+                  height="80vh"
+                  borderRadius={10}
+                  p={{ mobile: 5, tablet: 8 }}
+                >
+                  {children}
+                </ScrollBox>
+              </Glass>
+            </ThemeProvider>
+          </Box>
+          <Box minWidth="250px" opacity={{ mobile: 0, tablet: 1 }}>
+            <Glass id="toc" blur={3} overflow="hidden" p={4}>
+              <Headline id="test" fontSize={2}>
+                Table of Contents
+              </Headline>
+              <Button
+                as="a"
+                href="#test"
+                // icon={<BiTime />}
+                justifyContent="flex-start"
+              >
+                Heading 1
+              </Button>
+              <Button
+                as="a"
+                href="#test"
+                // icon={<BiTime />}
+                justifyContent="flex-start"
+              >
+                Heading 2
+              </Button>
+            </Glass>
+          </Box>
         </Box>
       </Box>
-    </Box>
+    </PageWrapper>
   );
 };
 
