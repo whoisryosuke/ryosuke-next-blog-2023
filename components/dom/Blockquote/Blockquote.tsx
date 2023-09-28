f5a217b798e8a09114c285e543fbaf93a503f1a8
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2023-09-27 17:14:56 -0700

    :bug: accomodate for line height

--- components/dom/Blockquote/Blockquote.tsx
+++ components/dom/Blockquote/Blockquote.tsx
@@ -6,6 +6,7 @@ const StyledBlockquote = styled("blockquote")`
   background-color: ${({ theme }) => theme.colors.glassOverlay};
   border-radius: ${({ theme }) => theme.radius[3]};
   padding: ${({ theme }) => theme.space[5]};
+  padding-bottom: ${({ theme }) => theme.space[3]};
   border: 0;
   margin: 0;
   margin-bottom: ${({ theme }) => theme.space[4]};
