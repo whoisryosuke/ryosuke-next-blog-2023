38fcd07425c76d2766dcd6a8ce9b1aba59853693
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2023-10-06 12:41:00 -0700

    :sparkles: pocketstation started

---
+++ components/3d/PocketStation/PocketStation.tsx
@@ -0,0 +1,80 @@
+import React, { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { Vector3 } from "three";
+import { animated, easings, useSpring } from "@react-spring/three";
+
+type Props = {};
+
+export default function PocketStation(props: Props) {
+  const frontPanelRef = useRef(null);
+  const { nodes, materials } = useGLTF("/models/PocketStation-v6.glb");
+
+  const { rotation } = useSpring({
+    config: { duration: 4200, easing: easings.easeInOutQuad },
+    delay: 4200,
+    loop: true,
+    from: {
+      rotation: [0, 0, 0],
+    },
+    to: [
+      {
+        rotation: [-1, 0, 0],
+      },
+      {
+        rotation: [0, 0, 0],
+      },
+    ],
+  });
+  return (
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.BodyBackComplete001.geometry}
+        material={materials.PS_Back}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.BodyFrontBase001.geometry}
+        material={materials.PS_FrontScreen}
+      />
+      <animated.mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.BodyFrontButtons.geometry}
+        material={materials["Material.030"]}
+        rotation={rotation}
+      />
+      <animated.mesh
+        ref={frontPanelRef}
+        castShadow
+        receiveShadow
+        geometry={nodes.BodyFrontPanel003.geometry}
+        material={materials.PS_FrontPanel}
+        rotation={rotation}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.CurveLanyardBaked.geometry}
+        material={materials.Lanyard}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.LanyardNotch.geometry}
+        material={materials.BlackPlastic}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.CurveLanyardTie001.geometry}
+        material={materials["BlackPlastic.001"]}
+      />
+    </group>
+  );
+}
+
+useGLTF.preload("/PocketStation-v1.glb");
---
+++ components/3d/PocketStationScene.tsx
@@ -0,0 +1,68 @@
+import {
+  ContactShadows,
+  Environment,
+  Lightformer,
+  MeshTransmissionMaterial,
+  OrbitControls,
+} from "@react-three/drei";
+import { GroupProps } from "@react-three/fiber";
+import { EffectComposer, N8AO, TiltShift2 } from "@react-three/postprocessing";
+import { UserCustomizations } from "@store/app";
+
+import { PropsWithChildren } from "react";
+import GlassCube from "./GlassCube";
+import PocketStation from "./PocketStation/PocketStation";
+import Canvas from "./Canvas";
+
+type Props = Partial<GroupProps> & {
+  customizations: UserCustomizations;
+};
+
+const FLOOR_HEIGHT = 15;
+
+const PocketStationScene = ({ customizations, ...props }: Props) => {
+  return (
+    <Canvas
+      camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 0, 10] }}
+      style={{ width: "100%", height: "100vh" }}
+    >
+      <Environment
+        // background
+        files="./images/neon_photostudio_1k.hdr"
+        {...props}
+      >
+        {/* Extra space lights if needed */}
+        <Lightformer
+          intensity={4}
+          position={[0, 7, 0]}
+          scale={[10, 30, 1]}
+          onUpdate={(self) => self.lookAt(0, 0, 0)}
+        />
+      </Environment>
+      {/* <color attach="background" args={["#e0e0e0"]} /> */}
+
+      {/* <spotLight position={[0,0,0]} penumbra={1} castShadow angle={0.2} /> */}
+      <rectAreaLight
+        position={[0, 0, -10]}
+        width={40}
+        height={40}
+        intensity={10}
+        color={"white"}
+        lookAt={() => [0, 0, 0]}
+      />
+
+      <PocketStation />
+
+      <OrbitControls
+        autoRotate={customizations.animation.active}
+        rotateSpeed={0.01}
+      />
+      {/*         
+        <EffectComposer disableNormalPass>
+          <N8AO aoRadius={1} intensity={2} />
+          <TiltShift2 blur={0.2} />
+        </EffectComposer> */}
+    </Canvas>
+  );
+};
+export default PocketStationScene;
