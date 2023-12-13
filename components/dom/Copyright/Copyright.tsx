d874f661ab994d978608619bad3c035b4e0dac09
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2023-12-12 16:22:20 -0800

    :fire: more old components + styles

--- components/dom/Copyright/Copyright.tsx
+++ components/dom/Copyright/Copyright.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import Box from "../Box/Box";
 import Text from "../Text/Text";
-import { copyrightStyles } from "./Copyright.css";
 
 type Props = {};
 
 const Copyright = (props: Props) => {
   return (
     <Box px={5} pt={5} pb={1}>
-      <Text fontSize={0} fontFamily="heading" className={copyrightStyles}>
+      <Text fontSize={0} fontFamily="heading">
         COPYRIGHT © 2019-{new Date().getFullYear().toString()}, RYOSUKE HANA
       </Text>
     </Box>
--- components/dom/List/ListItem.css.ts
+++
@@ -1,8 +0,0 @@
-import { style } from "@vanilla-extract/css";
-import { themes, base } from "../../theme";
-
-export const listItemStyles = style({
-  color: themes.tokens.colors.text,
-  marginBottom: base.tokens.space[2],
-  lineHeight: "3rem",
-});
