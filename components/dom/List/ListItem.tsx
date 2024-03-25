303a8cea70125aeb9718996d2b79f08d106332ea
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2024-03-25 15:27:13 -0700

    :lipstick: unify text styles

--- components/dom/List/ListItem.tsx
+++ components/dom/List/ListItem.tsx
@@ -1,15 +1,16 @@
 import React, { PropsWithChildren } from "react";
 import Box from "../Box/Box";
 import Text, { TextProps } from "../Text/Text";
+import Paragraph from "../Text/Paragraph";
 
 type Props = {};
 
 const ListItem = ({ children, ...props }: PropsWithChildren<Props>) => {
   return (
     <Box as="li" mb={4} color="text" {...props}>
-      <Text as="span" fontSize={3} lineHeight={6}>
+      <Paragraph as="span" marginBottom={0}>
         {children}
-      </Text>
+      </Paragraph>
     </Box>
   );
 };
